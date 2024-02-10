78f697d2f634873d9daa2d8c5d1ae650c376570b
JesusRamirez03 <jesusal.rg.03@gmail.com>
2024-02-09 19:39:14 -0600

    first commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,21 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {schema, rules} from '@ioc:Adonis/Core/Validator'
+import User from 'App/Models/User'
+
+export default class AuthController {
+
+  /*
+  public async index({}: HttpContextContract) {}
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}*/
+}
